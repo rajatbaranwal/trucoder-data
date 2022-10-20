2978020e7a8ab2ead1dc600a06b19c9afcd59116
Apoorv Srivastava <53645584+MaskedCarrot@users.noreply.github.com>
2022-10-20 14:00:46 +0530

    Create contest_scheduler.py

---
+++ contest_scheduler.py
@@ -0,0 +1,86 @@
+import requests
+import pytz
+from datetime import datetime
+import json
+
+
+class Contest:
+    def __init__(self, id, name, platform, link, startTime, endTime):
+        self.id = id
+        self.name = name
+        self.platform = platform
+        self.link = link
+        self.startTime = startTime
+        self.endTime = endTime
+
+
+
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
+epoch = datetime.utcfromtimestamp(0)
+
+def getTimeInMillis(time_str):
+    time_object = datetime.strptime(time_str, TIME_FORMAT)
+    return int((time_object-epoch).total_seconds()*1000)
+
+'''
+facebook: 133
+codeforces: 1
+codechef: 2
+leetcode: 102
+google: 35
+atcoder: 93
+'''
+def getPlatformIdFromResourceId(clistId):
+    if (clistId == 1):
+        return 1
+    elif (clistId == 2):
+        return 2
+    elif (clistId == 93):
+        return 3
+    elif (clistId == 102):
+        return 4
+    elif (clistId == 35):
+        return 5
+    elif (clistId == 133):
+        return 6
+    else:
+        return -1
+    
+def getPlatformNameFromResourceId(clistId):
+    if (clistId == 1):
+        return 'Codeforces'
+    elif (clistId == 2):
+        return 'Codechef'
+    elif (clistId == 93):
+        return 'Atcoder'
+    elif (clistId == 102):
+        return 'Leetcode'
+    elif (clistId == 35):
+        return 'Google'
+    elif (clistId == 133):
+        return 'Facebook'
+    else:
+        return 'Platform not supported'
+    
+
+URL = "https://clist.by:443/api/v2/contest/?username=MaskedCarrot&api_key=3756ead7ff87d60d0029be2c4d3b6847ad6aa1b5"
+
+data = requests.get(url = URL).json()
+processedData = {'contests': []}
+
+for d in data['objects']:    
+    if (getPlatformIdFromResourceId(d['resource_id']) == -1): 
+            continue
+    processedData['contests'].append(
+        Contest(
+            id=getPlatformIdFromResourceId(d['resource_id']),
+            name=d['event'],
+            platform=getPlatformNameFromResourceId(d['resource_id']),
+            link=d['href'],
+            startTime=getTimeInMillis(d['start']),
+            endTime=getTimeInMillis(d['end'])
+        ).__dict__
+    )
+    
+json_object = json.dumps(processedData, indent = 4) 
+print(json_object)
